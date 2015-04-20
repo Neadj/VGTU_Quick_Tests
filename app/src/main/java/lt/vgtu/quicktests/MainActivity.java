539e39c6738d3d9fc92f403951763b96dc1a3ca3
Aymeric Giraudet <neadj59@gmail.com>
2015-04-20 13:51:37 +0300

    Simple request to REST server, putting the result in a spinner

--- app/src/main/java/lt/vgtu/quicktests/MainActivity.java
+++ app/src/main/java/lt/vgtu/quicktests/MainActivity.java
@@ -1,17 +1,84 @@
 package lt.vgtu.quicktests;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    protected static String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
+        InputStream in = entity.getContent();
+        StringBuffer out = new StringBuffer();
+        int n = 1;
+        while (n > 0) {
+            byte[] b = new byte[4096];
+            n = in.read(b);
+            if (n > 0) out.append(new String(b, 0, n));
+        }
+        return out.toString();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        URI uri = null;
+        try {
+            uri = new URI("http://158.129.237.185:8080/rest/user/");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        HttpGet request = new HttpGet();
+        request.setURI(uri);
+        HttpResponse response;
+        ArrayList<String> list = new ArrayList<String>();
+        try {
+            StrictMode.ThreadPolicy policy = new
+                    StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            HttpClient client = new DefaultHttpClient();
+            response = client.execute(request);
+            HttpEntity en = response.getEntity();
+            String text = getASCIIContentFromEntity(en);
+
+            JSONArray Jo = new JSONArray(text);
+            for (int i = 0; i < Jo.length(); i++) {
+                JSONObject j = Jo.getJSONObject(i);
+                list.add(j.getString("title"));
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Spinner sp = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> ar = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        sp.setAdapter(ar);
     }
 
 

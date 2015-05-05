b062e0fe70a44ef54f5103a008b397fbee856711
Aymeric Giraudet <neadj59@gmail.com>
2015-05-05 13:43:59 +0300

    Complete test and get result at the end.

---
+++ app/src/main/java/lt/vgtu/quicktests/ResultActivity.java
@@ -0,0 +1,47 @@
+package lt.vgtu.quicktests;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class ResultActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        Intent intent = getIntent();
+        int correctAnswers = intent.getExtras().getInt("correct_answers");
+        int totalQuestions = intent.getExtras().getInt("total_questions");
+        int percentage = (int) ((double) correctAnswers / (double) totalQuestions * 100);
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText("You got " + correctAnswers + " correct answers out of " + totalQuestions +
+        ".\nWhich stands for " + percentage + "%.");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_result, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/lt/vgtu/quicktests/TestActivity.java
+++ app/src/main/java/lt/vgtu/quicktests/TestActivity.java
@@ -9,12 +9,14 @@ import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class TestActivity extends ActionBarActivity {
 
     private Test test;
     private int currentQuestion;
+    private int correctAnswers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,7 @@ public class TestActivity extends ActionBarActivity {
         setContentView(R.layout.activity_test);
         Intent intent = getIntent();
         currentQuestion = 0;
+        correctAnswers = 0;
         test = MainActivity.list.get(intent.getExtras().getInt("test_id"));
         Question q = test.getQuestions().get(0);
         TextView progress = (TextView) findViewById(R.id.progress);
@@ -29,26 +32,42 @@ public class TestActivity extends ActionBarActivity {
         TextView question = (TextView) findViewById(R.id.question);
         question.setText(q.getQuestion());
         RadioGroup rg = (RadioGroup) findViewById(R.id.radiogroup);
-        for(String s : q.getAnswers()) {
+        for(int i = 0; i < q.getAnswers().size(); i++) {
             RadioButton rb = new RadioButton(this);
+            rb.setId(i);
             rg.addView(rb);
-            rb.setText(s);
+            rb.setText(q.getAnswers().get(i));
         }
     }
 
     public void nextQuestion(View view) {
-        currentQuestion++;
-        TextView progress = (TextView) findViewById(R.id.progress);
-        progress.setText("Question " + (currentQuestion+1) + " of " + test.getQuestions().size());
-        Question q = test.getQuestions().get(currentQuestion);
-        TextView question = (TextView) findViewById(R.id.question);
-        question.setText(q.getQuestion());
         RadioGroup rg = (RadioGroup) findViewById(R.id.radiogroup);
-        rg.removeAllViews();
-        for(String s : q.getAnswers()) {
-            RadioButton rb = new RadioButton(this);
-            rg.addView(rb);
-            rb.setText(s);
+        if(rg.getCheckedRadioButtonId() == -1)
+            return;
+        if(currentQuestion < test.getQuestions().size()-1) {
+            if(rg.getCheckedRadioButtonId() == test.getQuestions().get(currentQuestion).getCorrectAnswer())
+                correctAnswers++;
+            currentQuestion++;
+            TextView progress = (TextView) findViewById(R.id.progress);
+            progress.setText("Question " + (currentQuestion + 1) + " of " + test.getQuestions().size());
+            Question q = test.getQuestions().get(currentQuestion);
+            TextView question = (TextView) findViewById(R.id.question);
+            question.setText(q.getQuestion());
+            rg.removeAllViews();
+            rg.check(-1);
+            for (int i = 0; i < q.getAnswers().size(); i++) {
+                RadioButton rb = new RadioButton(this);
+                rb.setId(i);
+                rg.addView(rb);
+                rb.setText(q.getAnswers().get(i));
+            }
+        } else {
+            if(rg.getCheckedRadioButtonId() == test.getQuestions().get(currentQuestion).getCorrectAnswer())
+                correctAnswers++;
+            Intent intent = new Intent(TestActivity.this, ResultActivity.class);
+            intent.putExtra("correct_answers", correctAnswers);
+            intent.putExtra("total_questions", test.getQuestions().size());
+            startActivity(intent);
         }
     }
 
